e82729085429afdbb54ac8e912b16dbdf48542df
Louie Rodriguez <lou3@lourodriguez.com>
2022-06-03 13:42:33 -0400

    new film

--- pullFilmsByDate.py
+++ pullFilmsByDate.py
@@ -12,7 +12,7 @@ import time
 
 start_time = time.time()
 
-dataPath = "C:\\Users\\louie\\OneDrive\\Desktop\\repo\\DeltekMap\\DeltekMapScirpts\\LBCode"
+dataPath = "C:\\Users\\louie\\OneDrive\\Desktop\\repo\\LetterboxdApp"
 # dataPath = "C:\\Users\\louie.rodriguez\\OneDrive - PENNONI\\Documents\\git\\DeltekMapScirpts\\LBCode"
 # user = "goldfishbrain"
 # user = "zacierka"
