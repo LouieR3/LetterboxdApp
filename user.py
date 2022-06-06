74ed4310f6745740d5214de48cc42919b6d5550d
Louie Rodriguez <lou3@lourodriguez.com>
2022-06-06 09:41:29 -0400

    dd

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 def user():
     # dataPath = "C:\\Users\\louie\\OneDrive\\Desktop\\repo\\LetterboxdApp"
     # dataPath = "C:\\Users\\louie.rodriguez\\OneDrive - PENNONI\\Documents\\git\\DeltekMapScirpts\\LBCode"
-    # user = "goldfishbrain"
-    user = "zacierka"
+    user = "goldfishbrain"
+    # user = "zacierka"
     # user = "bluegrace11"
     # user = "cloakenswagger"
     file = "AllFilms" + user + ".csv"
