8f26b29d967d8a71c219a2bae0a8fed9e87f8a57
louier3 <lou3@lourodriguez.com>
2022-09-21 23:42:19 -0400

    test redacted

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 def user():
     # dataPath = "C:\\Users\\louie\\Desktop\\repo\\LetterboxdApp"
     # dataPath = "C:\\Users\\louie.rodriguez\\OneDrive - PENNONI\\Documents\\git\\DeltekMapScirpts\\LBCode"
-    # user = "goldfishbrain"
-    user = "zacierka"
+    user = "goldfishbrain"
+    # user = "zacierka"
     # user = "bluegrace11"
     # user = "cloakenswagger"
     # user = "gr8escape10"
