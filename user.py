f89fa3ea5758d35447f98b950687bb938e707bb3
louier3 <lou3@lourodriguez.com>
2023-02-03 16:02:35 -0500

    ll

--- user.py
+++ user.py
@@ -1,11 +1,11 @@
 def user():
     # dataPath = "C:\\Users\\louie\\Desktop\\repo\\LetterboxdApp"
     # dataPath = "C:\\Users\\louie.rodriguez\\OneDrive - PENNONI\\Documents\\git\\DeltekMapScirpts\\LBCode"
-    # user = "goldfishbrain"
+    user = "goldfishbrain"
     # user = "zacierka"
     # user = "bluegrace11"
     # user = "cloakenswagger"
-    user = "gr8escape10"
+    # user = "gr8escape10"
     # user = "prahladsingh"
     # user = "carmal"
     file = "AllFilms" + user + ".csv"
