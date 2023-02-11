7979bcb5b8fc10b9f5810d51b5ad4dc4e2d26641
louier3 <lou3@lourodriguez.com>
2023-02-11 15:55:28 -0500

    percentile

--- chatgpt.py
+++ chatgpt.py
@@ -42,7 +42,7 @@ df250 = df250.reset_index(drop=True)
 df250['LBRating'] = (df250["LBRating"]*3)
 # df250['Length'] = (df250["MovieLength"]//10)*10
 # df250['decade'] = (df250["ReleaseYear"]//10)*10
-
+# print(round(len(df)*.1))
 total_num_ratings = df250["NumberOfRatings"].max()
 genre_weight = 0.4
 actor_weight = 0.4
