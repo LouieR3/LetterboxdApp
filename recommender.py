110ee68de63b2cb6af44335f81ff086b89ae28f3
louier3 <lou3@lourodriguez.com>
2023-02-11 15:57:27 -0500

    more weight to director

--- recommender.py
+++ recommender.py
@@ -55,7 +55,7 @@ def app():
     total_num_ratings = df250["NumberOfRatings"].max()
     genre_weight = 0.4
     actor_weight = 0.4
-    director_weight = 1.2
+    director_weight = 1.5
     length_weight = 0.8
     language_weight = 0.3
     decade_weight = 1
