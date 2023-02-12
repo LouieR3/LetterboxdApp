ae559c711a9fa6c922187b47af587f70aff70a79
louier3 <lou3@lourodriguez.com>
2023-02-12 10:54:14 -0500

    d

--- recommender.py
+++ recommender.py
@@ -184,7 +184,7 @@ def app():
     #     'Number of Ratings',
     #     'Actors'
     # ])
-
-    df3 = movies_df.style.background_gradient(subset=['Score', 'NumberOfRatings']).style.format("{:.2%}")
+    movies_df.style.format("{:.2%}")
+    df3 = movies_df.style.background_gradient(subset=['Score', 'NumberOfRatings'])
     # df3.index += 1 
     st.dataframe(df3, height=700, width=2000)
