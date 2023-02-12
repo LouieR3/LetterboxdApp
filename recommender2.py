a08a2729e7aaab54dec6742ca7db50740e871807
louier3 <lou3@lourodriguez.com>
2023-02-12 15:38:21 -0500

    das

--- recommender2.py
+++ recommender2.py
@@ -22,7 +22,7 @@ def app():
     option = 'cloakenswagger'
     option = st.selectbox(
         'Which user do you want to look at?',
-        ('cloakenswagger', 'carmal', 'prahladsingh', 'bluegrace11', 'gr8escape10', 'zacierka'))
+        ('cloakenswagger', 'carmal', 'prahladsingh', 'bluegrace11', 'gr8escape10', 'zacierka', 'goldfishbrain'))
 
     st.write('You selected:', option)
     file = user(option)
