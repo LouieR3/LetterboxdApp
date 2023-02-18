352fdd5618ebe45e4f4f6b891e92e60983e15e04
louier3 <lou3@lourodriguez.com>
2023-02-18 15:00:38 -0500

    sdf

--- streamlit2.py
+++ streamlit2.py
@@ -1,40 +1,85 @@
-
 import streamlit as st
+import pandas as pd
+# Define your cached function using st.cache_data
+@st.cache_data()
+def load_user_data(user):
+    # Load user data for the selected username
+    user_data = "AllFilms" + user + ".csv"
+     
+    return user_data
 
+# Define your Streamlit app pages as functions
+def homepage():
+    # Let the user select a username from a selectbox
+    username = st.selectbox("Select a username", ["goldfishbrain", "cloakenswagger", "carmal"])
+    
+    # Cache the user data for the selected username
+    user_data = load_user_data(username)
+    
+    # Display the user data on the homepage
+    st.write(f"User data for {username}:")
+    df = pd.read_csv(user_data)
+    df["Genre"] = df["Genre"].str.split(",")
+    df["Languages"] = df["Languages"].str.split(",")
+    df["Actors"] = df["Actors"].str.split(",")
+    df = df.drop(["MovieLength", "NumberOfReviews"], axis=1)
+    df = df.rename(columns={"MyRating": "Your Rating", "LBRating": "Letterboxd Rating", "ReviewDate": "Date Reviewed", "LengthInHour": "Movie Length", "Genre": "Genres", "NumberOfRatings": "Number Of Ratings", "ReleaseYear": "Release Year"})
+    # st.write(user_data)
+    st.dataframe(user_data, height=700, use_container_width=True)
 
-@st.cache
-def get_name():
-    return None
+def another_page():
+    # Get the selected username from the cached function
+    username = st.cache_data_lookup(load_user_data).args[0]
+    df = pd.read_csv(username)
+    df['MyRating'] = (df["MyRating"]*2)
+    df['length'] = (df["MovieLength"]//10)*10
+    # group the dataframe by user and length
+    user_length_group = df.groupby(["length"])
 
-def page1():
-    name = get_name() or "unknown"
-    st.write("Hello, {}! This is page 1.".format(name))
-    set_name()
+    # calculate the sum of ratings for each length seen by each user
+    length_sum_ratings = user_length_group["MyRating"].sum()
 
-def page2():
-    name = get_name() or "unknown"
-    st.write("Hello, {}! This is page 2.".format(name))
-    set_name()
+    # calculate the total number of movies seen by each user for each length
+    length_total_movies = user_length_group["Movie"].count()
 
-def page3():
-    name = get_name() or "unknown"
-    st.write("Hello, {}! This is page 3.".format(name))
-    set_name()
+    # calculate the average rating for each length seen by each user
+    length_avg_ratings = length_sum_ratings / length_total_movies
 
-def set_name():
-    name = st.text_input("What is your name?")
-    if name:
-        get_name.cache_clear()
-        get_name.set(name)
+    difference = user_length_group["Difference"].mean()
 
-# Define the pages and their associated functions
+    # create a dataframe with the average rating for each length seen by each user
+    length_ratings = pd.DataFrame({"Average Rating": length_avg_ratings, "Total Movies": length_total_movies, "Difference": difference})
+
+    # calculate the percentage of movies seen for each length by each user
+    length_ratings["Percentage"] = (length_ratings["Total Movies"] / len(df)) * 100
+
+    # define the weighting factor
+    weight = 0.95
+
+    # create a new column with the weighted sum of ratings and total_movies
+    length_ratings['Weighted Average'] = length_ratings['Average Rating']*weight + length_ratings['Total Movies']*(1-weight) + length_ratings['Difference']
+
+    # print the favorite length for user 1
+    # length_ratings= length_ratings.sort_values(by=['Weighted Average'], ascending=False)
+    # length_ratings["Ranking"] = range(1, len(length_ratings) + 1)
+    # length_ratings.insert(0, 'length', length_ratings.index)
+    # genre_ratings['Genre'] = genre_ratings.index
+    # length_ratings = length_ratings.set_index("Ranking")
+    length_ratings["Average Rating"] = length_ratings["Average Rating"]/2
+    df2 = length_ratings.style.background_gradient(subset=['Weighted Average']).format({"Difference": "{:.2f}","Average Rating": "{:.2f}","Percentage": "{:.2f}", 'Weighted Average': '{:.2f}'})
+    # df2.index += 1 
+    st.dataframe(df2, height=700, use_container_width=True)
+    # Display the selected username on another page
+    st.write(f"You selected the username: {username}")
+
+# Define your Streamlit app navigation
 PAGES = {
-    "Page 1": page1,
-    "Page 2": page2,
-    "Page 3": page3,
+    "Homepage": homepage,
+    "Another Page": another_page
 }
 
-# Define the sidebar navigation
-nav = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[nav]
+# Run the Streamlit app
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
 page()
