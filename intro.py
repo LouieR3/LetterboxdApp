f6760209e8f71cc578a187cb26689770e0abadf4
louier3 <lou3@lourodriguez.com>
2023-02-18 17:36:23 -0500

    idk

--- intro.py
+++ intro.py
@@ -5,24 +5,24 @@ def app():
 
     st.header('All Your Movies')
     # st.write('TO PUT HERE.....')
-    # options = ['cloakenswagger', 'carmal', 'prahladsingh', 'bluegrace11', 'gr8escape10', 'zacierka', 'goldfishbrain']
+    options = ['cloakenswagger', 'carmal', 'prahladsingh', 'bluegrace11', 'gr8escape10', 'zacierka', 'goldfishbrain']
 
-    # # option = 'cloakenswagger'
-    # st.selectbox('Which user do you want to look at?', options, key="my_selectbox", on_change=lambda value: st.session_state.update_option(value))
-        # print(option)
+    # option = 'cloakenswagger'
+    st.selectbox('Which user do you want to look at?', options, key="my_selectbox", on_change=lambda value: st.session_state.update_option(value))
+    # print(option)
     # Define a default value for the session variable
-    # if "selected_option" not in st.session_state:
-    #     st.session_state.selected_option = options[0]
-    option = st.text_input('Which user do you want to look at?', '')
-    if option:
-        st.write('You selected:', option)
-        file = user(option)
-        df = pd.read_csv(file)
-        df["Genre"] = df["Genre"].str.split(",")
-        df["Languages"] = df["Languages"].str.split(",")
-        df["Actors"] = df["Actors"].str.split(",")
-        df = df.drop(["MovieLength", "NumberOfReviews"], axis=1)
-        df = df.rename(columns={"MyRating": "Your Rating", "LBRating": "Letterboxd Rating", "ReviewDate": "Date Reviewed", "LengthInHour": "Movie Length", "Genre": "Genres", "NumberOfRatings": "Number Of Ratings", "ReleaseYear": "Release Year"}) 
-        pd.options.mode.chained_assignment = None
-        # df2 = df.style.background_gradient(subset=['Ranking', 'Billing Score'])
-        st.dataframe(df, height=700, use_container_width=True)
+    if "selected_option" not in st.session_state:
+        st.session_state.selected_option = options[0]
+        
+    st.write('You selected:', st.session_state.selected_option)
+
+    file = user(st.session_state.selected_option)
+    df = pd.read_csv(file)
+    df["Genre"] = df["Genre"].str.split(",")
+    df["Languages"] = df["Languages"].str.split(",")
+    df["Actors"] = df["Actors"].str.split(",")
+    df = df.drop(["MovieLength", "NumberOfReviews"], axis=1)
+    df = df.rename(columns={"MyRating": "Your Rating", "LBRating": "Letterboxd Rating", "ReviewDate": "Date Reviewed", "LengthInHour": "Movie Length", "Genre": "Genres", "NumberOfRatings": "Number Of Ratings", "ReleaseYear": "Release Year"}) 
+    pd.options.mode.chained_assignment = None
+    # df2 = df.style.background_gradient(subset=['Ranking', 'Billing Score'])
+    st.dataframe(df, height=700, use_container_width=True)
