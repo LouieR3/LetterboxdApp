284f36a353d7a2cdb6e079037ddbc9d46281ff63
louier3 <lou3@lourodriguez.com>
2023-03-07 13:09:32 -0500

    breaking

--- intro.py
+++ intro.py
@@ -6,7 +6,8 @@ def app():
 
     st.header('All Your Movies')
     # st.write('TO PUT HERE.....')
-    options = ['cloakenswagger', 'carmal', 'prahladsingh', 'bluegrace11', 'gr8escape10', 'zacierka', 'goldfishbrain']
+    options = ['cloakenswagger', 'carmal', 'prahladsingh',
+               'bluegrace11', 'gr8escape10', 'zacierka', 'goldfishbrain']
 
     # option = 'cloakenswagger'
     selectUser = st.selectbox('Which user do you want to look at?', options)
@@ -16,8 +17,9 @@ def app():
     if "my_global_variable" not in st.session_state:
         st.session_state.my_global_variable = "AllFilms" + options[0] + ".csv"
 
-    st.write('You selected:', st.session_state.my_global_variable.split("AllFilms")[1].split(".csv")[0])
-    
+    st.write('You selected:', st.session_state.my_global_variable.split(
+        "AllFilms")[1].split(".csv")[0])
+
     file = get_user()
     df = pd.read_csv(file)
     dfAvg = df[(df["MovieLength"] > 60) & (df["MovieLength"] < 275)]
@@ -26,15 +28,17 @@ def app():
     df["Genre"] = df["Genre"].str.split(",")
     df["Languages"] = df["Languages"].str.split(",")
     df["Actors"] = df["Actors"].str.split(",")
-    
+
     df = df.drop(["MovieLength", "NumberOfReviews"], axis=1)
     df.index = df.index + 1
-    df = df.rename(columns={"MyRating": "Your Rating", "LBRating": "Letterboxd Rating", "ReviewDate": "Date Reviewed", "LengthInHour": "Movie Length", "Genre": "Genres", "NumberOfRatings": "Number Of Ratings", "ReleaseYear": "Release Year"}) 
+    df = df.rename(columns={"MyRating": "Your Rating", "LBRating": "Letterboxd Rating", "ReviewDate": "Date Reviewed",
+                   "LengthInHour": "Movie Length", "Genre": "Genres", "NumberOfRatings": "Number Of Ratings", "ReleaseYear": "Release Year"})
     pd.options.mode.chained_assignment = None
     # df2 = df.style.background_gradient(subset=['Ranking', 'Billing Score'])
 
     avgRound = "{:.2f}".format(avg)
-    totalMovies = len(df)
-    st.write(f'Your average rating across all movies is: **{avgRound}** over **{totalMovies}** amount of movies')
+    totalMovies = len(ddf)
+    st.write(
+        f'Your average rating across all movies is: **{avgRound}** over **{totalMovies}** amount of movies')
 
     st.dataframe(df, height=700, use_container_width=True)
