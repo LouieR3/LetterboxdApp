2d51cd370c25394ef74b1a0e12ed6ec658998f82
louier3 <lou3@lourodriguez.com>
2023-07-10 16:36:03 -0400

    sfd

--- recommender2.py
+++ recommender2.py
@@ -74,8 +74,7 @@ def app():
     language_weight = 0.3
     decade_weight = 1
     popularity_weight = 0.4
-    rating_weight = 1.5
-    rating_weight = 1
+    rating_weight = 1.2
 
     def calculate_score(movies_df, fav_directors, fav_actors, fav_genres, fav_length, fav_decade, fav_language):
         scores = []
